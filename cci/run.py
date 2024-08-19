9fa5100fe00600a64c19e972bb9dc49f7a18ee83
Joseph C Wang <joequant@gmail.com>
2024-08-19 22:24:55 +0800

    add run

--- cci/run.py
+++ cci/run.py
@@ -11,13 +11,15 @@ if __name__ == "__main__":
    cci = cci.TwitterCci()
    while True:
       jobs = ic(adapter.read_recent_jobs())
-      daily_sentiment_cci, weighted_survey_score, combined_cci =  \
-        cci.compute_cci("U.S. Economy")
-      ic(adapter.write_result(daily_sentiment_cci))
-      print(f"Social Media Sentiment CCI Score: {daily_sentiment_cci}")
-      print(f"Survey CCI Score: {weighted_survey_score}")
-      print(f"Combined CCI Score: {combined_cci}")
-      time.sleep(30)
+      ic(jobs)
+      if len(jobs['result']) > 0:
+         daily_sentiment_cci, weighted_survey_score, combined_cci =  \
+            cci.compute_cci("U.S. Economy")
+         ic(adapter.write_result(daily_sentiment_cci))
+         print(f"Social Media Sentiment CCI Score: {daily_sentiment_cci}")
+         print(f"Survey CCI Score: {weighted_survey_score}")
+         print(f"Combined CCI Score: {combined_cci}")
+      time.sleep(5)
 
 
                 
